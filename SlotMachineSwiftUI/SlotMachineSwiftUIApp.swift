88f1951244b79bbcd91775588c80495ba99247d9
Khavi18 <khavishini18@gmail.com>
2024-07-09 20:30:45 +0800

    Initial Commit

---
+++ SlotMachineSwiftUI/SlotMachineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineSwiftUIApp.swift
+//  SlotMachineSwiftUI
+//
+//  Created by Khavishini on 09/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
